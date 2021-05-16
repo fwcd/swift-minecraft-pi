2dd06b3bea73e77f185631ab7d6b0986ac4b9893
fwcd <fwcdmail@gmail.com>
2021-05-16 18:35:55 +0200

    Update demo to output block hit events

--- Sources/MinecraftPiDemo/main.swift
+++ Sources/MinecraftPiDemo/main.swift
@@ -2,4 +2,8 @@ import MinecraftPi
 
 let mc = try! Minecraft.connect()
 
+// Post a message to the in-game chat
 mc.chat.post("Hello world!")
+
+// Print previous block events
+print(mc.events.block.hits)
