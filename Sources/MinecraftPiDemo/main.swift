ee50080b24b5985f2ae29b3abf9576dde3430783
fwcd <fwcdmail@gmail.com>
2021-05-16 20:33:47 +0200

    Update demo

--- Sources/MinecraftPiDemo/main.swift
+++ Sources/MinecraftPiDemo/main.swift
@@ -9,13 +9,21 @@ mc.chat.post("Hello world!")
 // Print the block the player is standing on.
 print()
 
-// Repeatedly poll information about the player
 while true {
-    Thread.sleep(forTimeInterval: 1)
+    Thread.sleep(forTimeInterval: 0.1)
 
-    let hits = mc.events.block.hits
     let belowPlayer = mc.world.getBlock(at: mc.player.tile - Vec3(y: 1))
     let pos = mc.player.pos
 
-    print("Hits: \(hits.count), block: \(belowPlayer.type), x: \(pos.x), y: \(pos.y), z: \(pos.z)")
+    // Erase the line
+    print("\u{1b}[2K\r", terminator: "")
+
+    // Print some info about the player
+    print("Block: \(belowPlayer.type), x: \(pos.x), y: \(pos.y), z: \(pos.z)", terminator: "")
+    fflush(stdout)
+
+    // Turn anything the player right-clicked with a sword into a gold block
+    for hit in mc.events.block.hits {
+        mc.world.setBlock(at: hit.pos, to: .goldBlock)
+    }
 }
